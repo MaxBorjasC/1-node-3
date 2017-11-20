01a1d33f15f233dbf7cd7209795d7af87c87289f
mborjas <borjascastillo@hotmail.com>
2017-11-19 23:35:35 -0500

    1 inicial

---
+++ config/secret.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+  database: process.env.DATABASE || 'mongodb://max:123@ds139262.mlab.com:39262/fiverrdatabase',
+  port: process.env.PORT || 3000,
+  secret: process.env.SECRET || 'fiverclone2222',
+
+}
+
